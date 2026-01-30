c773dff9d18578b84f1c6249be94380d5e35c64a
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-01-30 14:46:52 +0530

    Add Linked List Folder to DSA

---
+++ LINKED_LIST/linkedList.java
@@ -0,0 +1,18 @@
+package LINKED LIST;
+
+public class linkedList {
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        linkedList list = new linkedList(); // creating a new object for the class linkedList.
+        
+    }
+}
