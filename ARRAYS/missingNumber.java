8b9ed6b9abeb4affcd0af8de328c76cce2f03227
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-02-20 22:14:32 +0530

    Add optimal approach using sum method to find the missing number

--- ARRAYS/missingNumber.java
+++ ARRAYS/missingNumber.java
@@ -22,6 +22,17 @@ public class missingNumber {
         return -1;  
     }
 
+    public static int sumOptimalApproach(int a[], int n){
+        int sum = (n*(n+1))/2;
+
+        int s2 = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            s2 = s2 + a[i];
+        }
+
+        return (sum - s2);
+    }
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -34,7 +45,9 @@ public class missingNumber {
             a[i] = sc.nextInt();
         }
 
-        int missing = bruteMissingNumber(a, n);
+        /* int missing = bruteMissingNumber(a, n); */
+
+        int missing = sumOptimalApproach(a, n);
 
         System.out.println("Missing number is: " + missing);
 
