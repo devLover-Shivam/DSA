f98af57663b9e6b06d1910bc278dc4566cebd65c
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-02-20 22:27:28 +0530

    Add optimal approach using single for loop % xor operation to find the missing number.

--- ARRAYS/missingNumber.java
+++ ARRAYS/missingNumber.java
@@ -33,6 +33,22 @@ public class missingNumber {
 
         return (sum - s2);
     }
+
+    public static int xorOptimalApproach(int a[], int n){
+
+    int XOR1 = 0;
+    int XOR2 = 0;
+
+    for (int i = 0; i < n-1; i++) {
+        XOR2 = XOR2 ^ a[i];
+        XOR1 = XOR1 ^ (i+1);
+    }
+
+    XOR1 = XOR1 ^ n;
+
+    return XOR1 ^ XOR2;
+}
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -47,7 +63,9 @@ public class missingNumber {
 
         /* int missing = bruteMissingNumber(a, n); */
 
-        int missing = sumOptimalApproach(a, n);
+        /* int missing = sumOptimalApproach(a, n); */
+
+        int missing = xorOptimalApproach(a, n);
 
         System.out.println("Missing number is: " + missing);
 
