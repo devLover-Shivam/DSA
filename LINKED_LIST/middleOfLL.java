fcb0213ffc88bc68f4d9765b22e2db9e412d952e
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-06-22 20:17:23 +0530

    Add finding middle of a LL

---
+++ LINKED_LIST/middleOfLL.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.next = null;    
+    }  
+}
+
+public class middleOfLL {
+
+    
+    static Node findMiddle(Node head){
+        if(head==null || head.next == null) return head;
+        //BRUTE FORCE APPROACH
+       /*  int cnt = 0;
+        Node temp = head;
+        while(temp!=null){
+            cnt++;
+            temp = temp.next;
+        }
+
+        int middleNode = cnt/2+1;
+        temp = head;
+        while(temp != null){
+            middleNode = middleNode-1;
+            if(middleNode == 0) break;
+            temp = temp.next;
+        }
+        return temp; */
+
+        //OPTIMAL SOLUTION
+
+        Node fast = head;
+        Node slow = head;
+
+        while(fast!= null && fast.next!=null){
+            slow= slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        // Creating a sample linked list:
+        Node head = new Node(10);
+        head.next = new Node(2);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(50);
+
+        // Find the middle node
+        Node midNode = findMiddle(head);
+
+        // Display the value of the middle node
+        System.out.println("The middle node value is: " + midNode.data);
+    }
+}    
+
