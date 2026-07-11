36ceedf5fbd2f6ccabff162d98b277673298f5f3
Shivam Kumar Gupta <shivamkumargupta109@gmail.com>
2026-07-11 15:44:36 +0530

    Add better approach

---
+++ ARRAYS/MaximumSubarraySum/BetterApp/Main.java
@@ -0,0 +1,25 @@
+
+
+public class Main {
+
+    static int maxSubArray(int[] nums) {
+        int maxi = Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                maxi = Math.max(sum,maxi);
+            }
+            
+        }
+
+        return maxi;
+    }
+
+     public static void main(String[] args) {
+        int arr[] = {-2,-3,4,-1,-2,1,5,-3};
+        int ans = maxSubArray(arr);
+        System.out.println(ans);
+     }
+}
